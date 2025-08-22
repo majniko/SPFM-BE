4c6c8189f6402b69f9a461f169b0c24b1f3f3ed7
majniko <nikola.majerova@reas.cz>
2025-08-22 19:43:36 +0200

    finishing touches

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,13 @@ import { CoreModule } from '@core/core.module';
 import { IdentityModule } from '@identity/identity.module';
 import { FinanceModule } from '@finance/finance.module';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 @Module({
   imports: [
     CoreModule,
     IdentityModule,
     FinanceModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
